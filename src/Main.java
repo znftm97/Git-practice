7863bf7d47815aaac2c3e08c14e12ecaeeffc60a
jihoon <znftm97@naver.com>
2022-04-20 17:51:58 +0900

    git 초기화

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main branch");
+        System.out.println("main branch - 2");
+        System.out.println("main branch - 3");
+    }
+}
