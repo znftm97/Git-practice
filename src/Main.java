5bc532fdb31f4ff006944857ceada3b044629d8a
jihoon <znftm97@naver.com>
2022-04-20 17:32:17 +0900

    정리

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("main branch");
-        System.out.println("branch1 - 1");
-        System.out.println("branch1 - 2");
-
-        System.out.println("branch10 - 1");
-
-        System.out.println("branch20 - 1");
-        System.out.println("branch20 - 2");
-
     }
 }
