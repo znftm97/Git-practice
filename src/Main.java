bf26ecb8aa97ec85c6ba7f7f748728cb08c6f4aa
jihoon <znftm97@naver.com>
2022-07-04 19:23:51 +0900

    초기화

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main branch");
-        System.out.println("main branch - 2");
-        System.out.println("main branch - 3");
-
-        System.out.println("main branch - 4");
-        System.out.println("main branch - 5");
-
-        System.out.println("branch100 - 1");
-
-        System.out.println("main branch - 4");
-        System.out.println("main branch - 5");
-
-        System.out.println("git commit template test");
-        System.out.println("slack 연동 테스트");
-        System.out.println("slack 연동 테스트2");
     }
 }
