b54d36e6e33ada7595e0846718fb5110685516e3
jihoon <znftm97@naver.com>
2022-07-04 20:10:41 +0900

    초기화

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main1");
-        System.out.println("main2");
 
-        System.out.println("main3");
-
-        System.out.println("B-branch1");
-        System.out.println("B-branch2");
     }
 }
